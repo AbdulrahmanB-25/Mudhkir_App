a6f0552d6fbcdbec58a61951e51ba564964e3a95
MoAlhazmi <hazmi1moh@gmail.com>
2025-02-18 19:11:36 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/pages/add_dose.dart';
 import 'package:myapp/pages/login.dart';
 import 'package:myapp/pages/mainpage.dart';
 import 'package:myapp/pages/signup.dart';
 import 'package:myapp/pages/welcome.dart';
-
+import 'package:myapp/pages/dose_schedule.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,6 +23,8 @@ class MyApp extends StatelessWidget {
       "/login" : (context) => const Login(),
       "/signup" : (context) => const Signup(),
       "/mainpage" : (context) => const MainPage(),
+      "/dose_schedule" : (context) => const dose_schedule(),
+      "/add_dose" : (context) => const add_dose(),
     },
 
     );
